ecf9d2c25866df65f0c5979441e4cdd427368351
Ivan Pires <Ivan.PSouza@al.infnet.edu.br>
2026-03-03 14:39:52 -0300

    Combate aleatorio implementado

---
+++ Assets/Scripts/EncontroAleatorio.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncontroAleatorio : MonoBehaviour
+{
+
+    [Header("Identificação")]
+    [Tooltip("Dê um nome único. ex. Marmota_001")]
+    public string idUnico;
+
+    [Header("Formação Inimiga")]
+    [Tooltip("Arraste os PREFABS dos inimigos da aba PROJECT para cá")]
+    public List<GameObject> inimigos;
+
+    [Header("Chance de encontro")]
+    public int chanceMax = 1000;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            int numero = Random.Range(1, chanceMax + 1);
+            IniciadorBatalha iniciador = GetComponent<IniciadorBatalha>();
+
+            if (numero == 1)
+            {
+                Debug.Log("foi dessa vez");
+                if (iniciador != null)
+                {
+                    iniciador.DispararBatalha(collision.gameObject, idUnico, inimigos);
+                }
+            }
+            else
+            {
+                Debug.Log("Não foi dessa vez");
+            }
+        }
+    }
+}
